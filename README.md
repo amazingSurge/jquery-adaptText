a80fb4ee303a80ced33b676b69210242c077e080
Kaptinlin <Kaptinlin@gmail.com>
2013-04-15 16:38:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Adapttext
+
+A jQuery plugin that help rescale text depending on it's container width.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/amazingSurge/jquery-adaptText/master/dist/adaptText.min.js
+[max]: https://raw.github.com/amazingSurge/jquery-adaptText/master/dist/adaptText.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/adaptText.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/jquery.adaptText.js
@@ -0,0 +1,64 @@
+/*
+ * adaptText
+ * https://github.com/amazingSurge/jquery-adaptText
+ *
+ * Copyright (c) 2013 amazingSurge
+ * Licensed under the MIT license.
+ */
+
+(function (window, document, $, undefined) {
+  "use strict";
+
+  // Constructor
+  var AdaptText = $.AdaptText = function (element, options) {
+     // Attach element to the 'this' keyword
+    this.element = element;
+    this.$element = $(element);
+
+    // options
+    var meta_data = [];
+    $.each(this.$element.data(), function(k, v) {
+        var re = new RegExp("^adaptText", "i");
+        if (re.test(k)) {
+            meta_data[k.toLowerCase().replace(re, '')] = v;
+        }
+    });
+    this.options = $.extend(true, {}, AdaptText.defaults, options, meta_data);
+
+    var self = this;
+    $.extend(self, {
+      init: function(){
+
+      }
+    });
+
+    self.init();
+  };
+
+  // Default options for the plugin as a simple object
+  AdaptText.defaults = {
+    maxFontSize: null,
+    minFontSize: null
+  };
+
+  // Collection method.
+  $.fn.adaptText = function(options) {
+      if (typeof options === 'string') {
+          var method = options;
+          var method_arguments = arguments.length > 1 ? Array.prototype.slice.call(arguments, 1) : undefined;
+
+          return this.each(function() {
+              var api = $.data(this, 'adaptText');
+              if (typeof api[method] === 'function') {
+                  api[method].apply(api, method_arguments);
+              }
+          });
+      } else {
+          return this.each(function() {
+              if (!$.data(this, 'adaptText')) {
+                  $.data(this, 'adaptText', new AdaptText(this, options));
+              }
+          });
+      }
+  };
+}(window, document, jQuery));
